bf01f8be84483cdc99ab9b0f010ab826a149cddb
Parva <45422029+Crzy633@users.noreply.github.com>
2025-01-17 18:12:40 +0800

    修

--- auth.config.ts
+++ auth.config.ts
@@ -6,10 +6,8 @@ export const authConfig = {
     },
     callbacks: {
         authorized({ auth, request: { nextUrl } }) {
-            console.log("authorized~~~");
             const isLoggedIn = !!auth?.user;
             const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
-            console.log("isLoggedIn", isLoggedIn, "isOnDashboard", isOnDashboard);
             if (isOnDashboard) {
                 if (isLoggedIn) return true;
                 return false; // Redirect unauthenticated users to login page
@@ -20,5 +18,4 @@ export const authConfig = {
         },
     },
     providers: [], // Add providers with an empty array for now
-    secret: 'ZS1dHrruKGlKJj5c5Ag3tRzlT0LZZBGTmIbKP1pSYBg=',
 } satisfies NextAuthConfig;
